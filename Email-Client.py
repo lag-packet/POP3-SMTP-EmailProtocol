f605e6a5075c38471c143644bc56efe99c42e2ee
lag-packet <36144253+lag-packet@users.noreply.github.com>
2023-03-27 09:35:39 -0400

    Add files via upload

---
+++ Email-Client.py
@@ -0,0 +1,64 @@
+import socket
+
+# Set up client constants
+SMTP_PORT = 25
+POP3_PORT = 110
+HOST = 'localhost'
+
+
+# Define SMTP functions
+def smtp_client():
+    # Connect to SMTP server socket
+    smtp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    smtp_client_socket.connect((HOST, SMTP_PORT))
+
+    # Receive SMTP greeting from server
+    data = smtp_client_socket.recv(1024)
+    print(data.decode())
+
+    # Send EHLO command to start SMTP handshake
+    smtp_client_socket.send(b'EHLO localhost\r\n')
+
+    # Receive response from server
+    data = smtp_client_socket.recv(1024)
+    print(data.decode())
+
+    # Close SMTP connection
+    smtp_client_socket.close()
+
+
+# Define POP3 functions
+def pop3_client():
+    # Connect to POP3 server socket
+    pop3_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    pop3_client_socket.connect((HOST, POP3_PORT))
+
+    # Receive POP3 greeting from server
+    data = pop3_client_socket.recv(1024)
+    print(data.decode(), end='')
+
+    # Send USER command to start POP3 handshake
+    pop3_client_socket.send(b'USER username\r\n')
+    data = pop3_client_socket.recv(1024)
+    print(data.decode(), end='')
+
+    # Send PASS command to continue POP3 handshake
+    pop3_client_socket.send(b'PASS password\r\n')
+    data = pop3_client_socket.recv(1024)
+    print(data.decode(), end='')
+
+    # Close POP3 connection
+    pop3_client_socket.close()
+    print('client closed')
+
+
+# Start SMTP and POP3 clients
+smtp_client() # Currently only connection code.
+pop3_client() # Currently only connection code.
+
+# Run Loop
+    # create connection ^ ^, send email
+    # ||
+    # create connection ^ ^, check email
+# Close connection
+
