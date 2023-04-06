8338d4b107892a20addc6712dbd5351cde16e42c
lag-packet <36144253+lag-packet@users.noreply.github.com>
2023-04-06 18:17:26 -0400

    rev 5
    hei

--- Email-Client.py
+++ Email-Client.py
@@ -1,6 +1,6 @@
 import socket
 import urllib.request
-
+import os
 
 # Set up client constants
 SMTP_PORT = 25
@@ -33,7 +33,7 @@ CLIENT_IP = get_public_ipv4()
 
 
 # Define SMTP functions
-def smtp_client(receiver, subject, message):
+def smtp_client(sender, receiver, subject, message):
     # Connect to SMTP server socket
     smtp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     smtp_client_socket.connect((HOST, SMTP_PORT))
@@ -48,7 +48,7 @@ def smtp_client(receiver, subject, message):
     print(data.decode())
 
     # Send MAIL FROM command to SMTP Server
-    smtp_client_socket.send('MAIL FROM: <{}>\r\n'.format(USERNAME).encode())
+    smtp_client_socket.send('MAIL FROM: <{}>\r\n'.format(sender).encode())
     data = smtp_client_socket.recv(1024)
     print(data.decode())
 
@@ -115,25 +115,136 @@ def pop3_client():
     pop3_client_socket.close()
     print('client closed')
 
+"""
+def user_exists(user_input):
+    if os.path.isdir(user_input):
+        return True
+    else:
+        return False
 
-authenticate = False
+
+def register_user(user_input, password_input):
+    # make user directory
+    os.mkdir(user_input)
+
+    # make credentials file
+    with open(user_input + '/credentials.txt', 'w') as file:
+        file.write('username:' + user_input + '\n')
+        file.write('password:' + password_input + '\n')
+
+    # make mail folder
+    os.mkdir(user_input + '/mail')
+
+    return True
+
+
+def run_authenticate(user_input, password_input):
+    credentials_file = user_input + '/credentials.txt'
+    print(f"credentials_file: {credentials_file}")
+    print('running authenticate!')
+
+    # check if the cred file exists
+    if not os.path.isfile(credentials_file):
+        print('file doesnt exist!')
+        return False
+
+    print('file exists')
+    # read stored creds from file
+    with open(credentials_file, 'r') as file:
+        print('opening to read creds!')
+        stored_username = file.readline().strip().split(':')[1]
+        print(f'stored user: {stored_username}')
+        stored_pass = file.readline().strip().split(':')[1]
+        print(f'stored pass: {stored_pass}')
+
+    # compare stored cred to input
+    if user_input == stored_username and password_input == stored_pass:
+        return True
+    else:
+        return False
+
+
+authenticated = False
+register = False
 while True:
-    if not authenticate:
+    if not authenticated or not register:
+        start_choice = input('Register or Login (R/L): ')
+
+    if start_choice.lower() == 'r':
+        register = True
+
+    if register:
+        while True:
+            username = input('Enter Desired User Name: ')
+            if user_exists(username):
+                print('User already exists. Please choose another username.')
+            else:
+                password = input('Enter Desired Password:')
+                register_user(username, password)
+                register = False
+                break
+
+    if start_choice == 'l' and not authenticated:
         choice = input("Enter User:")
-        USERNAME = choice + '@' + CLIENT_DOMAIN
+        USERNAME = choice
         choice = input("Enter Password:")
         PASSWORD = choice
         # authenticate METHOD here TODO
-        authenticate = True
+        while True:
+            if run_authenticate(USERNAME, PASSWORD):
+                break
+            else:
+                print('Wrong info!')
+                choice = input("Enter User:")
+                USERNAME = choice
+                choice = input("Enter Password:")
+                PASSWORD = choice
+        authenticated = True
         print(f"Welcome {USERNAME}")
 
-    choice = input("Enter 'send' or 'mail':")
-    if choice == 'send':
-        smtp_client('test@348.edu', 'yo yo', 'this is a test message!')
+    if authenticated:
+        choice = input("Enter 'send' or 'mail':")
+        if choice == 'send':
+            sender = USERNAME
+            receiver = input('To user: ')
+            subject = input('Enter email subject: ')
+            message = input('Enter email message: ')
+            while True:
+                if not user_exists(username):
+                    print('sent.')
+                    break
+                else:
+                    print('sent')
+                    break
+            smtp_client('test@348.edu', 'yo yo', 'this is a test message!')
+        if choice == 'mail':
+            print('implement pop here')
+"""
+
+user_inputted = False
+done = False
+while not done:
+    if not user_inputted:
+        username_input = input('Enter Username: ')
+        if '@' not in username_input:
+            print("please enter a valid email!")
+            continue
+        print(f'Hello {username_input}!')
+
+    user_inputted = True
+    y_n = input('Do you wish to send (Y/N): ')
+    if y_n.lower() == 'y':
+        receiver = input('Enter receiver: ')
+        subject = input('Enter subject: ')
+        message = input('Enter message: ')
+        smtp_client(username_input, receiver, subject, message)
+    else:
+        print('Bye!')
+        done = True
+        break
 
 # Run Loop
-    # create connection ^ ^, send email
-    # ||
-    # create connection ^ ^, check email
+# create connection ^ ^, send email
+# ||
+# create connection ^ ^, check email
 # Close connection
-
